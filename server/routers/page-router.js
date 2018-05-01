c6086fd10059080a11045a2d7b2c0473f7077ba5
Saurabh Chouhan <schouhan@aripratech.com>
2018-05-02 04:09:10 +0530

    index page would not be rendered on all application url to ensure that all router url are handled and do not show page not found

--- server/routers/page-router.js
+++ server/routers/page-router.js
@@ -3,15 +3,16 @@ import Router from 'koa-router'
 
 const pageRouter = new Router()
 
-pageRouter.get("/", (ctx) => {
+pageRouter.get('/logout', function (ctx) {
+  ctx.logout()
+  ctx.redirect('/')
+})
+
+pageRouter.get("/*", (ctx) => {
     return ctx.render("index", {})
 })
 
 
-pageRouter.get('/logout', function (ctx) {
-    ctx.logout()
-    ctx.redirect('/')
-})
 
 pageRouter.use(function (ctx, next) {
     if (ctx.isAuthenticated()) {
