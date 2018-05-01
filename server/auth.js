09675fae076d2e85f6479bff5f414a294e20fd6d
Saurabh Chouhan <schouhan@aripratech.com>
2018-05-02 04:00:09 +0530

    Modified passport local strategy to make email as login field rather than username

--- server/auth.js
+++ server/auth.js
@@ -3,30 +3,32 @@ import LocalStrategy from 'passport-local'
 import {UserModel} from "./models"
 import co from 'co'
 
-const strategy = new LocalStrategy(
-    (username, password, done) => {
-        co(async () => {
-            try {
-                let user = await UserModel.verifyUser(username, password)
-                if (user)
-                    return done(null, user)
-                else
-                    return done(null, false, {message: 'Login failed'})
+const strategy = new LocalStrategy({
+    usernameField: 'email'
+  },
+  (username, password, done) => {
+    co(async () => {
+      try {
+        let user = await UserModel.verifyUser(username, password)
+        if (user)
+          return done(null, user)
+        else
+          return done(null, false, {message: 'Login failed'})
 
-            } catch (ex) {
-                return done(null, false, {message: 'Login failed'})
-            }
-        })
-    }
+      } catch (ex) {
+        return done(null, false, {message: 'Login failed'})
+      }
+    })
+  }
 )
 
-passport.use(strategy);
+passport.use(strategy)
 
 // you can use this section to keep a smaller payload
 passport.serializeUser(function (user, done) {
-    done(null, user)
+  done(null, user)
 })
 
 passport.deserializeUser(function (user, done) {
-    done(null, user)
+  done(null, user)
 })
